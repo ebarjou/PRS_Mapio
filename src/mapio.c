effe4a9fe7f1cc5293bae32295a8ec99f345844d
Christophe Caubet <ccaubet001@emi.u-bordeaux1.fr>
2016-11-23 09:24:58 +0100

    map_save presque finie

--- src/mapio.c
+++ src/mapio.c
@@ -40,10 +40,57 @@ void map_save (char *filename)
 {
   // TODO
   int fd = open(filename, O_WRONLY || O_CREAT || O_TRUNC, 0666);
-  int nb_obj = map_objects();
-  write(fd, &nb_obj, sizeof(int));
-  printf("%d\n", map_objects());
-  // Trouver comment récupérer la liste des objets utilisés.
+
+  // Find and write the sizes of the map
+  int width = map_width();
+  int height = map_height();
+  write(fd, &width, sizeof(int));
+  write(fd, &height, sizeof(int));
+
+  // Find and write the different objects used in the map
+  int nb_obj_diff = map_objects();
+  int objs_diff[nb_obj_diff]; // The object types "found already"
+  for (int i = 0 ; i < nb_obj_diff ; i++) objs_diff[i] = -1; // Initialization
+  int cpt = 0; // Number of object types found
+  for (int y = 0 ; y < height ; y++) { // For each square in the map
+    for (int x = 0 ; x < width ; x++) {
+      int obj_temp;
+      if((obj_temp = map_get(x, y)) != MAP_OBJECT_NONE) { // If its object isn't empty
+        int new_obj = 0;
+        for (int i = 0 ; i < cpt ; i++) { // And we don't know it yet
+          if (obj_temp == objs_diff[cpt]) {
+            new_obj = 1;
+            break;
+          }
+        }
+        if (new_obj == 0) {
+          objs_diff[cpt++] = obj_temp;
+        }
+      }
+    }
+  }
+  // Write the number of objects so it know how many have to be read
+  write(fd, &cpt, sizeof(int));
+  for(int i = 0 ; i < cpt ; i++) { // Then write the caracteristics of the objects
+    int obj = objs_diff[i];
+    char* map_name = map_get_name(obj);
+    int value = sizeof(map_name);
+    write(fd, &value, sizeof(int)); // The length of the filename
+    write(fd, map_name, value); // Then, the filename
+    unsigned value_uns = map_get_frames(obj);
+    write(fd, &value_uns, sizeof(unsigned)); // The number of frames/sprites
+    value = map_get_solidity(obj);
+    write(fd, &value, sizeof(int)); // The solidity of the object (0,1 or 2)
+    value = map_is_destructible(obj);
+    write(fd, &value, sizeof(int)); // The destructability of the object (0 or 1)
+    value = map_is_collectible(obj);
+    write(fd, &value, sizeof(int)); // The collectibility of the object (0 or 1)
+    value = map_is_generator(obj);
+    write(fd, &value, sizeof(int)); // The generability of the object (0 or 1)
+  }
+
+  // TODO : write le contenu de chaque case de la carte
+
   fprintf (stderr, "Sorry: Map save is not yet implemented\n");
   close(fd);
 }
