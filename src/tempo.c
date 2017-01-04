95aaa60dde785040fb3a2ed87876b4c6d146b29e
ebarjou <e.barjousuire@gmail.com>
2017-01-04 15:58:39 +0100

    Avancement d de tempo.c (fonctionnel), reste à regler pb avec le thread daemon

--- src/tempo.c
+++ src/tempo.c
@@ -28,18 +28,59 @@ static unsigned long get_time (void)
 
 typedef struct evenement {
 	void *param;
+	unsigned t;
+	
 } event;
-event* current_event;
+int current_event;
+
+event** next_events;
+int event_size;
 
 pthread_t daemon;
 
-void traitant_event(int sig){
-	// TODO 4.3 + verifier que 4.1 et 4.2 sont fonctionnels
-	if (pthread_self() != daemon) return;
+int set_current_event()
+{
+	/*printf("Liste des timers : \n");
+	for (int i = 0; i < event_size; ++i){
+		if (next_events[i] != NULL){
+			printf("\t%d dans %dus\n",i,  next_events[i]->t - get_time());
+		} else {
+			printf("\t%d (vide)\n",i);
+		}
+		
+	}*/
+	event* closest = NULL;
+	for (int i = 0; i < event_size; ++i){
+		if (next_events[i] != NULL && (closest == NULL || closest->t > next_events[i]->t)) {
+			current_event = i;
+			closest = next_events[i];
+		}
+	}
+	if (closest != NULL){
+		unsigned t = closest->t - get_time();
+		if (t <= 0) t = 0;
+		struct itimerval timer;
+		timer.it_value.tv_sec = (int)((double)t/1000000.0);
+		timer.it_value.tv_usec = t%1000000;
+		timer.it_interval.tv_sec = 0;
+		timer.it_interval.tv_usec = 0;
+		if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
+			perror("Erreur à l'appel de setitimer()");
+			exit(1);
+		}
+	}
+	return 1;
+}
 
-	printf("Je suis le thread n%lu\n", (long unsigned int)daemon);
-	printf ("sdl_push_event(%p) appelee au temps %ld\n",
-		current_event->param, get_time ());
+void traitant_event(int sig){
+	/*
+	 TODO: Le signal n'est pas géré par le thread daemon mais par le thread principal
+	*/
+	//if (pthread_self() != daemon) return;
+	sdl_push_event(next_events[current_event]->param);
+	free(next_events[current_event]);
+	next_events[current_event] = NULL;
+	set_current_event();
 }
 
 void *catch_alrms(void *p) {
@@ -50,37 +91,49 @@ void *catch_alrms(void *p) {
 	s.sa_handler = traitant_event;
 	sigemptyset(&s.sa_mask);
 	s.sa_flags=0;
-	while (1) {
+	//while (1) {
 		sigaction(SIGALRM, &s, NULL);
 		sigsuspend(&masque);
-	}
+	//}
 	return NULL;
 }
 
 // timer_init returns 1 if timers are fully implemented, 0 otherwise
 int timer_init (void)
 {
-	// V�rifier que 4.1 est fonctionnel
 	pthread_create(&daemon, NULL, catch_alrms, NULL);
-	return 0; // Implementation not ready
+	event_size = 10;
+	next_events = malloc(sizeof(event*)*event_size);
+	for (int i = 0; i < event_size; ++i) next_events[i] = NULL;
+	return 1;
+}
+
+int add_timer(event *e)
+{
+	for (int i = 0; i < event_size; ++i){
+		if (next_events[i] == NULL) {
+			next_events[i] = e;
+			return 1;
+		}
+	}
+	event **rlc_events = (event*)malloc(sizeof(event*)*event_size*2);
+	for (int i = 0; i < event_size*2; ++i) rlc_events[i] = NULL;
+	for (int i = 0; i < event_size; ++i) rlc_events[i] = next_events[i];
+	event_size *= 2;
+	free(next_events);
+	next_events = rlc_events;
+	add_timer(e);
+	return 1;
 }
 
 void timer_set (Uint32 delay, void *param)
 {
-	// TODO 4.3 + v�rifier que 4.2 est fonctionnel
 	event *new_event = malloc(sizeof(event));
 	new_event->param = param;
-	current_event = new_event;
-	
-	struct itimerval timer;
-	timer.it_value.tv_sec = delay/1000;
-	timer.it_value.tv_usec = (delay*1000) % 1000000;
-	timer.it_interval.tv_sec = 0;
-	timer.it_interval.tv_usec = 0;
-	if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
-		perror("Erreur � l'appel de setitimer()");
-		exit(1);
-	}
+	new_event->t = get_time()+delay*1000;
+	add_timer(new_event);
+
+	set_current_event();
 }
 
 #endif
